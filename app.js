e7c0cee128045e4c71ea0e23bcaefeaac6a9cc41
mamuka111 <123620275+mamuka111@users.noreply.github.com>
2024-05-21 16:20:43 +0400

    adad

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const circle = document.querySelector(".circle1");
 const circle2 = document.querySelector(".circle2");
 const iconsDiv = document.querySelector(".iconsDiv");
 const startButton = document.querySelector(".custom-btn-warning ");
+const paper2 = document.querySelector(".paper2");
+const scissors2 = document.querySelector(".scissors2");
+const rock2 = document.querySelector(".rock2");
 
 startButton.addEventListener("click", function () {
   startButton.style.display = "none";
@@ -32,5 +35,29 @@ window.addEventListener("click", function (event) {
   }
 });
 button.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+paper2.addEventListener("click", function () {
+  const clonedElement1 = paper2.cloneNode(true);
+  clonedElement1.classList.remove("paper2");
+  clonedElement1.classList.add("paper2Clone");
+  clonedElement1.style.display = "block";
+  circle.parentNode.replaceChild(clonedElement1, circle);
+  modal.style.display = "none";
+});
+scissors2.addEventListener("click", function () {
+  const clonedElement2 = scissors2.cloneNode(true);
+  clonedElement2.classList.remove("scissors2");
+  clonedElement2.classList.add("scissors2Clone");
+  clonedElement2.style.display = "block";
+  circle.parentNode.replaceChild(clonedElement2, circle);
+  modal.style.display = "none";
+});
+rock2.addEventListener("click", function () {
+  const clonedElement3 = rock2.cloneNode(true);
+  clonedElement3.classList.remove("rock2");
+  clonedElement3.classList.add("rock2Clone");
+  clonedElement3.style.display = "block";
+  circle.parentNode.replaceChild(clonedElement3, circle);
   modal.style.display = "none";
 });
