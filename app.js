26a8d558ebcf3d65fc858de9e7639c1eed42935e
mamuka111 <123620275+mamuka111@users.noreply.github.com>
2024-05-21 22:42:04 +0400

    adadad

--- app.js
+++ app.js
@@ -12,6 +12,33 @@ const startButton = document.querySelector(".custom-btn-warning ");
 const paper2 = document.querySelector(".paper2");
 const scissors2 = document.querySelector(".scissors2");
 const rock2 = document.querySelector(".rock2");
+const submitButton = document.querySelector(".submitButton");
+const restartButton = document.querySelector(".restartButton");
+const rockPaperScissors = [paper2, scissors2, rock2];
+const yourScore = document.querySelector(".yourScore");
+const CpuScore = document.querySelector(".CpuScore");
+let isButtonClicked = false;
+
+function activateHover() {
+  circle.classList.add("hover-effect");
+  setTimeout(() => {
+    circle.classList.remove("hover-effect");
+  }, 1000); // Remove the hover effect after 300 milliseconds
+}
+
+submitButton.addEventListener("click", function () {
+  if (!document.contains(circle)) {
+    const random = Math.floor(Math.random() * 3);
+    let clonedElement4 = rockPaperScissors[random].cloneNode(true);
+    clonedElement4.className = "";
+    clonedElement4.classList.add("rock2Clone");
+    circle2.parentNode.replaceChild(clonedElement4, circle2);
+    isButtonClicked = true;
+    console.log(random);
+  } else if (document.contains(circle)) {
+    activateHover();
+  }
+});
 
 startButton.addEventListener("click", function () {
   startButton.style.display = "none";
@@ -20,16 +47,19 @@ startButton.addEventListener("click", function () {
   paper.style.display = "none";
   circle.style.display = "block";
   circle2.style.display = "block";
+  submitButton.style.display = "block";
+  restartButton.style.display = "block";
+  yourScore.style.display = "block";
+  CpuScore.style.display = "block";
   iconsDiv.style.gap = "150px";
   console.log("Button clicked");
 });
+
 circle.addEventListener("click", function () {
   modal.style.display = "block"; // Show modal
 });
 
 window.addEventListener("click", function (event) {
-  //   console.log("Clicked on:", event.target);
-  //   console.log("Modal:", modal);
   if (event.target === modal) {
     modal.style.display = "none";
   }
